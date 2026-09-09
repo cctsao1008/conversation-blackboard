4b1cc3b44a9172d713bffa55d6f5007cea6f09cf
Cheng <chiacheng.tsao@gmail.com>
2026-09-09 22:41:26 +0800

    Make shared API models deserializable for Rust client

--- src/model.rs
+++ src/model.rs
@@ -1,13 +1,13 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Identity {
     pub source: String,
     pub instance: String,
     pub label: Option<String>,
 }
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Message {
     pub id: i64,
     pub created_at: i64,
@@ -19,7 +19,7 @@ pub struct Message {
     pub reply_to: Option<i64>,
 }
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ChannelSummary {
     pub channel: String,
     pub message_count: i64,
